5917a68c1128400ecf0c16d48b199e4089ce8809
Ivar Nilsen <post@ivarnilsen.com>
2016-08-14 09:07:45 +0200

    React with no throttled scroll event

---
+++ src/react-scroll-event-no-throttle/index.js
@@ -0,0 +1,70 @@
+import 'babel-polyfill';
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import { setup } from '~/scene';
+import { setup as frontpage } from '~/frontpage';
+
+class Scroller extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onScroll = this.onScroll.bind(this);
+        this.state = { y: 0 };
+    }
+
+    componentWillMount() {
+        document.addEventListener('scroll', this.onScroll);
+    }
+
+    componentWillDismount() {
+        document.removeEventListener('scroll', this.onScroll);
+    }
+
+    onScroll() {
+        const y = window.scrollY;
+        this.setState({ y });
+    }
+
+    onBackLinkClick(event) {
+        event.preventDefault();
+        history.pushState({}, 'Scroll animations', '/');
+        frontpage();
+        return false;
+    }
+
+    render() {
+        const { y } = this.state;
+
+        return (
+            <div className="main">
+                <a
+                    className="back-link"
+                    href="/"
+                    onClick={this.onBackLinkClick}
+                >
+                    Back
+                </a>
+                {[1,2,3].map(index => (
+                    <div
+                        key={`node-${index}`}
+                        className={`appear-on-scroll appear-on-scroll--${index}`}
+                        style={{
+                            transform: `translateX(-${y}px`,
+                        }}
+                    />
+                ))}
+            </div>
+        );
+    }
+
+}
+
+export default () => {
+
+    render(
+        <Scroller />,
+        document.getElementById('root')
+    );
+}
